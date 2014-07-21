2595f43c066703dbea00353391184476145c67e2
hila.karimov <hila.karimov@kaltura.com>
2014-07-21 12:17:12 +0300

    New ffmpeg version

---
+++ ffmpeg-2.1.3-bin/ffmpeg-2.1.3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#_MODULE_DIR_=/root/build/ffmpeg-1.1.1/ffmpeg-1.1.1-bin
+_FULLPATH_=$(readlink -f $0)
+_MODULE_DIR_=$(dirname $_FULLPATH_)
+LD_LIBRARY_PATH=$_MODULE_DIR_ $_MODULE_DIR_/ffmpeg "$@"
+
+
